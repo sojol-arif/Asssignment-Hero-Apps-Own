b55a044464222a2e25024f8ad6215624a9a66d14
Arif <absojol@gmail.com>
2026-02-07 19:22:41 +0600

    Finally I try to do it more perfect design

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -19,8 +19,8 @@ const Banner = () => {
                 </h1>
                 <p className='text-[#627382] text-[16px] lg:text-[20px] leading-[1.4] pt-4'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.<br></br>Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
                 <div className='btn_box flex-wrap justify-center flex gap-4 my-9 lg:my-14'>
-                    <Link to="https://play.google.com/store/games?hl=en" className='flex flex-wrap items-center gap-2 rounded-[4px] border border-[#D2D2D2] px-6 py-3 text-[18px] lg:text-[20px] text-[#001931] font-semibold hover:shadow-[0_1px_5px_2px_rgba(0,0,0,0.1)] transition duration-300'> <img src={googAppBtn}/> Google Play</Link>
-                    <Link to="https://www.apple.com/app-store/" className='flex flex-wrap items-center gap-2 rounded-[4px] border border-[#D2D2D2] px-6 py-3 text-[18px] lg:text-[20px] text-[#001931] font-semibold hover:shadow-[0_1px_5px_2px_rgba(0,0,0,0.1)] transition duration-300'>  <img src={appStoreBtn}/> App Store</Link>
+                    <Link to="https://play.google.com/store/games?hl=en" className='flex flex-wrap items-center gap-2 rounded-[4px] border border-[#D2D2D2] px-6 py-3 text-[18px] lg:text-[20px] text-[#001931] font-semibold hover:shadow-[0_5px_10px_-2px_rgba(0,0,0,0.1)] transition duration-300'> <img src={googAppBtn}/> Google Play</Link>
+                    <Link to="https://www.apple.com/app-store/" className='flex flex-wrap items-center gap-2 rounded-[4px] border border-[#D2D2D2] px-6 py-3 text-[18px] lg:text-[20px] text-[#001931] font-semibold hover:shadow-[0_5px_10px_-2px_rgba(0,0,0,0.1)] transition duration-300'>  <img src={appStoreBtn}/> App Store</Link>
                 </div>
                 <div className='iphpne_box flex flex-wrap gap-[40px]'>
                     <div className='w-[100%] lg:w-[200px] flex flex-col gap-[40px] items-center lg:items-start'> 
